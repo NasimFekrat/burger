9099952bd6d7be335100cf9530cc933aef71f838
Nasim <nasimfekrat@yahoo.com>
2019-01-10 18:07:49 -0500

    Added!

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Burger", true);
